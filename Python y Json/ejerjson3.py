f145432dc284653958301b1e404539771b63c438
velazco07 <36158209+velazco07@users.noreply.github.com>
2018-06-11 16:40:36 +0200

    Delete ejerjson3.py

--- Python y Json/ejerjson3.py
+++
@@ -1,9 +0,0 @@
-# Dime el nombre y el id del/los equipo/s que estan ubicados en Philadelphia
-import json
-from pprint import pprint
-with open("nba.json") as data_file:
-	data = json.load(data_file)
-for equipo in data:
-	if equipo["location"] == "Philadelphia":
-		print("Nombre:", equipo["teamName"])
-		print("Id:", equipo["teamId"])
