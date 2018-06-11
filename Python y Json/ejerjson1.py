c41278c21bec41c6bf6f080f337cbf09a04a2cff
velazco07 <36158209+velazco07@users.noreply.github.com>
2018-06-11 16:39:43 +0200

    Delete ejerjson1.py

--- Python y Json/ejerjson1.py
+++
@@ -1,7 +0,0 @@
-# Dime el nombre de todos los equipos.
-import json
-from pprint import pprint 
-with open("nba.json") as data_file:
-	data = json.load(data_file)
-for equipo in data:
-	print(equipo["teamName"])
